a765499723bf139f91fd5be88658ac4fec56ff56
Nek Phung <doanphung13579@gmail.com>
2026-07-25 14:27:11 +0700

    Xoa vinh vien

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -105,3 +105,12 @@ module.exports.changeMulti = async (req, res) => {
     // console.log(ids);
     res.redirect(req.get("Referrer") || "/");
 }
+
+// [DELETE] /admin/products/delete/:id
+module.exports.deleteItem = async (req, res) => {
+    const id = req.params.id;
+
+    await Product.deleteOne( { _id: id });
+    res.redirect(req.get("Referrer") || "/");
+    // console.log(req.params);
+}
