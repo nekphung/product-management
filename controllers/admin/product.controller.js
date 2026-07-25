b1faf7df03b8b39607ec83aed850eb6468250221
Nek Phung <doanphung13579@gmail.com>
2026-07-25 14:44:34 +0700

    Xoa mem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -110,7 +110,11 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
     const id = req.params.id;
 
-    await Product.deleteOne( { _id: id });
+    // await Product.deleteOne( { _id: id });
+    await Product.updateOne({_id: id}, {
+        deleted: true,
+        deletedAt: new Date() // cái này lấy thời gian thực
+    });
+
     res.redirect(req.get("Referrer") || "/");
-    // console.log(req.params);
 }
