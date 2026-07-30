606e227800cbb6ff0ea0d02cdc92d1e7db5f76bc
Nek Phung <doanphung13579@gmail.com>
2026-07-30 16:53:51 +0700

    Them slug, tao moi san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -4,6 +4,8 @@ const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search")
 const paginationHelper = require("../../helpers/pagination")
 
+const systemConfig = require("../../config/system");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
     // console.log(req.query.status);
@@ -150,3 +152,29 @@ module.exports.deleteItem = async (req, res) => {
     res.redirect(req.get("Referrer") || "/");
 }
 
+// [GET] /admin/products/create
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/products/create", {
+        pageTitle: "Thêm mới sản phẩm",
+    });
+}
+
+// [POST] /admin/products/create
+module.exports.createPost = async (req, res) => {
+    req.body.price = parseInt(req.body.price);
+    req.body.discountPercentage = parseInt(req.body.discountPercentage);
+    req.body.stock = parseInt(req.body.stock);
+
+    if (req.body.position == "") {
+        const countProducts = await Product.countDocuments();
+        req.body.position = countProducts + 1;
+        // console.log(countProducts);
+    } else {
+        req.body.position = parseInt(req.body.position);
+    }
+
+    const product = new Product(req.body);
+    await product.save(); 
+
+    res.redirect(`${systemConfig.prefixAdmin}/products`);
+}
