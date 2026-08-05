446982b30fce79267a8ae5e607384743b3ff1ac7
Nek Phung <doanphung13579@gmail.com>
2026-08-05 23:38:29 +0700

    Them nhom quyen

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,8 +1,10 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/products-category.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search")
 const paginationHelper = require("../../helpers/pagination")
+const createTreeHelper = require("../../helpers/createTree")
 
 const systemConfig = require("../../config/system");
 
@@ -167,8 +169,17 @@ module.exports.deleteItem = async (req, res) => {
 
 // [GET] /admin/products/create
 module.exports.create = async (req, res) => {
+    const find = {
+        deleted: false,
+    }
+
+    const category = await ProductCategory.find(find);
+
+    const newCategory = createTreeHelper.tree(category);
+
     res.render("admin/pages/products/create", {
         pageTitle: "Thêm mới sản phẩm",
+        category: newCategory
     });
 }
 
@@ -217,9 +228,16 @@ module.exports.edit = async (req, res) => {
         const product = await Product.findOne(find);
         // console.log(product);
 
+        const category = await ProductCategory.find({
+            deleted: false
+        });
+
+        const newCategory = createTreeHelper.tree(category);
+
         res.render("admin/pages/products/edit", {
             pageTitle: "Chỉnh sửa sản phẩm",
-            product: product 
+            product: product,
+            category: newCategory
         });
     } catch (error) {
         res.redirect(`${systemConfig.prefixAdmin}/products`);
