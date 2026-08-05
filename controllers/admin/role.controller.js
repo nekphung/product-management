d54d6bfc8b642e816febb79b299d3f759a4eadfd
Nek Phung <doanphung13579@gmail.com>
2026-08-06 00:44:45 +0700

    Xong phan nhom quyen

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -32,3 +32,40 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
 }
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        let find = {
+            _id: id,
+            deleted: false
+        };
+
+        const data = await Role.findOne(find);
+
+        // console.log(data);
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Chỉnh sửa nhóm quyền",
+            data: data
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/roles`)
+    }
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await Role.updateOne({_id: id}, req.body);
+
+        req.flash("success", "Cập nhật nhóm quyền thành công!");
+
+        res.redirect(req.get("Referrer") || "/");
+    } catch (error) {
+        req.flash("error", "Cập nhật nhóm quyền thất bại!");
+    }
+}
