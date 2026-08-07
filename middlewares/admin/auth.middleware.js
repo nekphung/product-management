433157750b92f31e30337be15fee3ad6d4ec1cd7
Nek Phung <doanphung13579@gmail.com>
2026-08-07 16:28:20 +0700

    Them phan authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,4 +1,5 @@
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 const systemConfig = require("../../config/system");
 
@@ -7,12 +8,17 @@ module.exports.requireAuth = async (req, res, next) => {
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
         // console.log(req.cookies.token);
-        const user = await Account.findOne({token: req.cookies.token});
+        const user = await Account.findOne({token: req.cookies.token}).select("-password");
         
         if (!user) {
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
         } else {
-            // console.log(user);
+            const role = await Role.findOne({
+                _id: user.role_id
+            }).select("title permissions");
+            
+            res.locals.user = user;
+            res.locals.role = role;
             next();
         }
     }
