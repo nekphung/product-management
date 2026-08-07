f8e3503a9211750dd71a889b8a0688c6ec0ab52e
Nek Phung <doanphung13579@gmail.com>
2026-08-07 07:51:01 +0700

    Trang danh sach san pham

---
+++ routes/admin/account.route.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+
+const controller = require("../../controllers/admin/account.controller");
+const upload = multer();
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+const validate = require("../../validates/admin/account.validate");
+
+router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+    "/create", 
+    upload.single("avatar"),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.createPost
+);
+
+module.exports = router;
