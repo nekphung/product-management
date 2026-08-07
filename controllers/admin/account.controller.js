f7ff7730c8a70a98e705b9c55cf7a91c3ae654b9
Nek Phung <doanphung13579@gmail.com>
2026-08-07 10:17:59 +0700

    Chinh sua tai khoan

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -60,3 +60,61 @@ module.exports.createPost = async (req, res) => {
     }
 }
 
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+    const id = req.params.id;
+
+    let find = {
+        _id: id,
+        deleted: false
+    };
+
+    try {
+        const data = await Account.findOne(find);
+        const roles = await Role.find({
+            deleted: false
+        })
+
+        // console.log(data);
+        res.render("admin/pages/accounts/edit", {
+            pageTitle: "Chỉnh sửa tài khoản",
+            data: data,
+            roles: roles,
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/accounts`)
+    }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const emailExist = await Account.findOne({
+            _id: { $ne: id },
+            email: req.body.email,
+            deleted: false
+        });
+
+        // console.log(emailExist);
+        if (emailExist) {
+            req.flash("error", `Email ${req.body.email} đã tồn tại`);
+        } else {
+            // Neu nguoi dung khong muon cap nhat mat khau thi khong nhap vao password
+            if (req.body.password) {
+                req.body.password = md5(req.body.password);
+            } else {
+                delete req.body.password;
+            }
+
+            await Account.updateOne({_id: id}, req.body);
+
+            req.flash("success", "Cập nhật tài khoản thành công!");
+        }
+
+        res.redirect(req.get("Referrer") || "/");
+        
+    } catch (error) {
+        req.flash("error", "Cập nhật tài khoản thất bại!");
+    }
+}
