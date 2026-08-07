f0cec1543b981762caaaab4e2ef60fb24a877cd0
Nek Phung <doanphung13579@gmail.com>
2026-08-07 14:27:19 +0700

    Phan dang nhap

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -13,24 +13,33 @@ module.exports.login = (req, res) => {
 module.exports.loginPost = async (req, res) => {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-        req.flash("error", "Vui lòng nhập email và mật khẩu.");
-        return res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
-    }
-
     const account = await Account.findOne({ email: email, deleted: false });
 
-    if (!account || account.password !== md5(password)) {
-        req.flash("error", "Email hoặc mật khẩu không đúng.");
-        return res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    if (!account) {
+        req.flash("error", "Email không tồn tại!");
+        res.redirect(req.get("Referrer") || "/");
+        return;
     }
 
-    req.session.user = {
-        id: account._id,
-        email: account.email,
-        fullName: account.fullName,
-        role_id: account.role_id
-    };
+    if (md5(password) != account.password) {
+        req.flash("error", "Sai mật khẩu!");
+        res.redirect(req.get("Referrer") || "/");
+        return;
+    }
+
+    if (account.status == "inactive") {
+        req.flash("error", "Tài khoản đã bị khóa!");
+        res.redirect(req.get("Referrer") || "/");
+        return;
+    }
+
+    res.cookie("token", account.token);
+    // req.session.user = {
+    //     id: account._id,
+    //     email: account.email,
+    //     fullName: account.fullName,
+    //     role_id: account.role_id
+    // };
 
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
 }
---
+++ validates/admin/auth.validate.js
@@ -0,0 +1,15 @@
+module.exports.loginPost = (req, res, next) => {
+    if (!req.body.email) {
+        req.flash("error", "Vui lòng nhập email!");
+        res.redirect(req.get("Referrer") || "/");
+        return;
+    }
+
+    if (!req.body.password) {
+        req.flash("error", "Vui lòng nhập mật khẩu!");
+        res.redirect(req.get("Referrer") || "/");
+        return;
+    }
+
+    next(); // next sang buoc ke tiep 
+}
