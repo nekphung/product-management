e08c6d986697c719abf991b04c5d6f4d910836f6
Nek Phung <doanphung13579@gmail.com>
2026-08-17 17:39:29 +0700

    Go back

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,15 @@ if (showAlert) {
 }
 
 // End Show Alert 
+
+// Button Go Back 
+const buttonGoBack = document.querySelectorAll("[button-go-back]");
+if (buttonGoBack.length > 0) {
+    buttonGoBack.forEach(button => {
+        button.addEventListener("click", () => {
+            history.back();
+        })
+    })
+}
+
+// End Button Go Back 
