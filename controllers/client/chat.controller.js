698de1e37b1f3c683e2026de2bb98aeb19f6a859
Nek Phung <doanphung13579@gmail.com>
2026-09-01 17:43:10 +0700

    Hoan thanh phan chat co ban

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,13 +1,48 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 
 // [GET] /chat/
 module.exports.index = async (req, res) => {
-  // SocketIO
-  _io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+  const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
+
+  // SocketIO, dùng once để kết nối một lần duy nhất
+  _io.once('connection', (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      // Luu vao database
+      const chat = new Chat({
+        user_id: userId,
+        content: content
+      });
+
+      await chat.save();
+
+      // Tra data ve client
+      _io.emit("SERVER_RETURN_MESSAGE", {
+        userId: userId,
+        fullName: fullName,
+        content: content
+      })
+    })
   });
 
   // End SocketIO
+
+  // Lay ra data
+  const chats = await Chat.find({
+    deleted: false
+  });
+
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id
+    }).select("fullName");
+
+    chat.infoUser = infoUser;
+  }
+
   res.render("client/pages/chat/index", {
-    pageTitle: "Chat"
+    pageTitle: "Chat",
+    chats: chats
   });
 };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,55 @@
-var socket = io();
+const chatBody = document.querySelector(".chat .inner-body");
+
+const scrollChatToBottom = (behavior = "auto") => {
+    if (!chatBody) return;
+    requestAnimationFrame(() => {
+        chatBody.scrollTo({
+            top: chatBody.scrollHeight,
+            behavior
+        });
+    });
+};
+
+scrollChatToBottom();
+
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+if (formSendData) {
+    formSendData.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const content = e.target.elements.content.value;
+        console.log(content);
+        if (content) {
+            socket.emit("CLIENT_SEND_MESSAGE", content);
+            e.target.elements.content.value = "";
+        }
+    })
+}
+
+// END CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+    // console.log(data);
+    const myId = document.querySelector("[my-id]").getAttribute("my-id");
+    const body = chatBody;
+    if (!body) return;
+
+    console.log(myId);
+
+    let htmlFullName = "";
+    const div = document.createElement("div");
+    if (myId == data.userId) {
+        div.classList.add("inner-outgoing");
+    } else {
+        div.classList.add("inner-incoming");
+        htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+    }
+    div.innerHTML = `
+        ${htmlFullName}
+        <div class="inner-content">${data.content}</div>
+    `;
+    body.appendChild(div);
+    scrollChatToBottom("smooth");
+})
+// END SERVER_RETURN_MESSAGE
